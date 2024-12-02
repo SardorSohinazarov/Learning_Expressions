bb745e5755f6d0913cb12c1e75b5a0d438dc1879
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-12-03 01:28:33 +0500

    Optimize uchun

---
+++ Expressions_2/ExpressionOptimizer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+
+public class ExpressionOptimizer : ExpressionVisitor
+{
+    protected override Expression VisitBinary(BinaryExpression node)
+    {
+        // Chap va o'ng qismlarni optimallashtirish
+        var left = Visit(node.Left);
+        var right = Visit(node.Right);
+
+        // Takrorlanayotgan shartlarni olib tashlash
+        if (left is ConstantExpression leftConst && right is ConstantExpression rightConst)
+        {
+            // Agar ikkala qism ham bo'sh yoki teng bo'lsa, optimallashtirilgan holda
+            bool leftValue = Convert.ToBoolean(leftConst.Value);
+            bool rightValue = Convert.ToBoolean(rightConst.Value);
+
+            return Expression.Constant(leftValue && rightValue);
+        }
+
+        // AND (&&) yoki OR (||) bilan takroriy shartlarni chiqarib tashlash
+        if (node.NodeType == ExpressionType.AndAlso && left == right)
+        {
+            return left; // Takrorlanadigan qismni olib tashlash
+        }
+
+        return Expression.MakeBinary(node.NodeType, left, right);
+    }
+
+    protected override Expression VisitMethodCall(MethodCallExpression node)
+    {
+        // MethodCallExpression uchun, masalan, Contains optimallashtirish
+        if (node.Method.Name == "Contains" && node.Object != null && node.Arguments.Count == 1)
+        {
+            var memberExp = (MemberExpression)node.Object;
+            var argument = (ConstantExpression)node.Arguments[0];
+
+            // Agar izlanayotgan qiymat bo'sh bo'lsa, shartni olib tashlash
+            if (argument.Value is string searchString && string.IsNullOrWhiteSpace(searchString))
+            {
+                return Expression.Constant(true);
+            }
+        }
+
+        return base.VisitMethodCall(node);
+    }
+}
--- Expressions_2/Program.cs
+++ Expressions_2/Program.cs
@@ -1,13 +1,15 @@
 ﻿using Expressions_2;
 using System.Linq.Expressions;
 
-Expression<Func<Person, bool>> expression = p => (p.Age > 18 && p.Age <= 70) && p.Name.Contains("Ali");
+Expression<Func<Person, bool>> expression = p => (p.Age > 18 && p.Age < 70) && p.Name.Contains("Ali");
 
+var optimizedExpression = new ExpressionOptimizer().Visit(expression);
+expression = (Expression<Func<Person, bool>>)optimizedExpression;
 var queryBuilder = new SqlQueryBuilder<Person>();
 string sqlQuery = queryBuilder.Translate(expression);
 
 Console.WriteLine(sqlQuery);
-// Natija: SELECT * FROM People WHERE (Age > 18 AND Name = 'Ali')
+//SELECT * FROM Person WHERE (((Age > 18) AND (Age <= 70)) AND Name LIKE '%' + 'Ali' + '%')
 
 public class Person
 {
