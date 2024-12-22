acb73793e049ad6f74caa3976a9eeb153a0cbb4e
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-12-22 20:25:20 +0500

    Cal culculator dotnet add package NCalc package orqali

---
+++ Expressions_6_Calculator/Calculator.cs
@@ -0,0 +1,111 @@
+﻿/*using Expression = NCalc.Expression;
+
+namespace Expressions_6_Calculator
+{
+    public class Calculator
+    {
+        public string InputText { get; set; }
+        private string inputText = "";
+
+        public string CalculatedResult { get; set; }
+        private string calculatedResult = "0";
+
+        private bool isSciOpWaiting = false;
+
+        private void Reset()
+        {
+            CalculatedResult = "0";
+            InputText = "";
+            isSciOpWaiting = false;
+        }
+
+        private void Calculate()
+        {
+            if (InputText.Length == 0)
+                return;
+
+            if (isSciOpWaiting)
+            {
+                InputText += ")";
+                isSciOpWaiting = false;
+            }
+
+            try
+            {
+                var inputString = NormalizeInputString();
+                Expression expression = new Expression(inputString);
+                var result = expression.Evaluate();
+
+                CalculatedResult = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                CalculatedResult = "NaN";
+            }
+        }
+
+        private string NormalizeInputString()
+        {
+            Dictionary<string, string> _opMapper = new()
+        {
+            {"×", "*"},
+            {"÷", "/"},
+            {"SIN", "Sin"},
+            {"COS", "Cos"},
+            {"TAN", "Tan"},
+            {"ASIN", "Asin"},
+            {"ACOS", "Acos"},
+            {"ATAN", "Atan"},
+            {"LOG", "Log"},
+            {"EXP", "Exp"},
+            {"LOG10", "Log10"},
+            {"POW", "Pow"},
+            {"SQRT", "Sqrt"},
+            {"ABS", "Abs"},
+        };
+
+            var retString = InputText;
+
+            foreach (var key in _opMapper.Keys)
+            {
+                retString = retString.Replace(key, _opMapper[key]);
+            }
+
+            return retString;
+        }
+
+        private void Backspace()
+        {
+            if (InputText.Length > 0)
+                InputText = InputText.Substring(0, InputText.Length - 1);
+        }
+
+        private void NumberInput(string key) => InputText += key;
+
+        private void MathOperator(string op)
+        {
+            if (isSciOpWaiting)
+            {
+                InputText += ")";
+                isSciOpWaiting = false;
+            }
+
+            InputText += $" {op} ";
+        }
+
+        private void RegionOperator(string op)
+        {
+            if (op == ")")
+                isSciOpWaiting = false;
+
+            InputText += op;
+        }
+
+        private void ScientificOperator(string op)
+        {
+            InputText += $"{op}(";
+            isSciOpWaiting = true;
+        }
+    }
+}
+*/
---
+++ Expressions_6_Calculator/Program.cs
@@ -0,0 +1,107 @@
+﻿using Expression = NCalc.Expression;
+
+namespace Expressions_6_Calculator
+{
+    public class Calculator
+    {
+        public string InputText { get; set; }
+        public string CalculatedResult { get; set; }
+        private bool isSciOpWaiting = false;
+
+        public Calculator()
+        {
+            InputText = "";
+            CalculatedResult = "0";
+        }
+
+        public void Reset()
+        {
+            CalculatedResult = "0";
+            InputText = "";
+            isSciOpWaiting = false;
+        }
+
+        public void Calculate()
+        {
+            if (InputText.Length == 0)
+                return;
+
+            if (isSciOpWaiting)
+            {
+                InputText += ")";
+                isSciOpWaiting = false;
+            }
+
+            try
+            {
+                var inputString = NormalizeInputString();
+                Expression expression = new Expression(inputString);
+                var result = expression.Evaluate();
+                CalculatedResult = result.ToString();
+            }
+            catch (Exception)
+            {
+                CalculatedResult = "NaN";
+            }
+        }
+
+        private string NormalizeInputString()
+        {
+            Dictionary<string, string> _opMapper = new()
+            {
+                {"×", "*"},
+                {"÷", "/"},
+                {"SIN", "Sin"},
+                {"COS", "Cos"},
+                {"TAN", "Tan"},
+                {"ASIN", "Asin"},
+                {"ACOS", "Acos"},
+                {"ATAN", "Atan"},
+                {"LOG", "Log"},
+                {"EXP", "Exp"},
+                {"LOG10", "Log10"},
+                {"POW", "Pow"},
+                {"SQRT", "Sqrt"},
+                {"ABS", "Abs"},
+            };
+
+            var retString = InputText;
+
+            foreach (var key in _opMapper.Keys)
+            {
+                retString = retString.Replace(key, _opMapper[key]);
+            }
+
+            return retString;
+        }
+
+        public void StartConsoleApp()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Kalkulyator (Chiqish uchun 'exit' deb yozing)");
+                Console.WriteLine("Joriy ifoda: " + InputText);
+                Console.WriteLine("Natija: " + CalculatedResult);
+                Console.WriteLine("Hisoblashni amalga oshirish uchun formulani kiriting:");
+
+                string userInput = Console.ReadLine();
+
+                if (userInput.ToLower() == "exit")
+                    break;
+
+                InputText = userInput;
+                Calculate();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            calculator.StartConsoleApp();
+        }
+    }
+}
